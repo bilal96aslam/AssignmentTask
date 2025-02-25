68699555f72d63ca48ae418216727e77564d5484
bilal96aslam <bilalaslamch96@gmail.com>
2025-02-25 11:45:08 +0500

    repo test case add for the room db

--- app/src/test/java/com/app/assignmenttask/presentation/BookRepositoryTest.kt
+++ app/src/test/java/com/app/assignmenttask/presentation/BookRepositoryTest.kt
@@ -1,15 +1,21 @@
 package com.app.assignmenttask.presentation
 
+import app.cash.turbine.test
+import com.app.assignmenttask.data.local.BookEntity
 import com.app.assignmenttask.data.local.FavouriteBookDao
+import com.app.assignmenttask.data.local.mappers.toBook
 import com.app.assignmenttask.data.remote.ApiService
 import com.app.assignmenttask.data.remote.base.ApiResponse
 import com.app.assignmenttask.data.remote.response.BookResponse
 import com.google.gson.JsonSyntaxException
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.mock
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import retrofit2.Response
 
@@ -74,6 +80,42 @@ class BookRepositoryTest {
         val response = result as ApiResponse.Error
         assertThat(response.error.code).isEqualTo(500)
         assertThat(response.error.message).contains("JSON parsing error")
+    }
+
+    @Test
+    fun `returns favouriteBooks when favourite book list available`(): Unit = runTest {
+        val expectedResponse = BookEntity("1", "Book 1", "John", "", "2", null, emptyList())
+        whenever(favouriteBookDao.getFavouriteBooks()).thenReturn(flowOf(listOf(expectedResponse)))
+        val result = bookRepository.getFavouriteBooks()
+        result.collect {
+            assertEquals(1, it.size)
+            assertEquals("Book 1", it[0].title)
+        }
+    }
+
+    @Test
+    fun `return true if book isFavourite`(): Unit = runTest {
+        val expectedResponse = BookEntity("1", "Book 1", "John", "", "2", null, emptyList())
+        whenever(favouriteBookDao.getFavouriteBooks()).thenReturn(flowOf(listOf(expectedResponse)))
+        val result = bookRepository.isBookFavourite("1")
+        result.test {
+            assertEquals(true, awaitItem())
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `mark book as favourite`(): Unit = runTest {
+        val expectedResponse = BookEntity("1", "Book 1", "John", "", "2", null, emptyList())
+        bookRepository.markAsFavourite(expectedResponse.toBook())
+        verify(favouriteBookDao).upsert(expectedResponse)
+    }
+
+
+    @Test
+    fun `delete book as favourite`(): Unit = runTest {
+        bookRepository.deleteFromFavourite("1")
+        verify(favouriteBookDao).deleteFavouriteBook("1")
     }
 
 }
